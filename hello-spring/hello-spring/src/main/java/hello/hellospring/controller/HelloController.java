eb11bbbe541ce5d7ef8a2513d979d40f733ffb7d
son <sonjm3518@dsm.hs.kr>
2021-09-03 13:32:24 +0900

    21 09 03

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -12,6 +13,12 @@ public class HelloController {
        model.addAttribute("data", "HELLO");
        model.addAttribute("data2", "BYE");
        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model ){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
 
