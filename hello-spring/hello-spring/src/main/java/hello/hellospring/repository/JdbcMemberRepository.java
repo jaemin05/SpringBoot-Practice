06167b5948222082e96d3f7abf1b768d3e57b218
son <sonjm3518@dsm.hs.kr>
2021-09-13 11:19:13 +0900

    21 09 13

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+
+        Connection connection = dataSource.getConnection();
+
+        connection.prepareStatement(sql);
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,11 +2,12 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
+    Member save(Member member) throws SQLException;
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
