9a5e81a9d49c3dc3c4523c22d6a113aedb4172a9
son <sonjm3518@dsm.hs.kr>
2021-09-14 20:29:45 +0900

    21 09 14

--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,13 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member) throws SQLException;
+    Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
