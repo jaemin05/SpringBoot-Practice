fde5db3c5199c579e5a6511073549a24cc3281d7
son <sonjm3518@dsm.hs.kr>
2021-09-24 17:27:19 +0900

    21 09 24

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //로깅을 사용하기 위한 어노테이션
 public class ArticleController {
 
     @Autowired //스프링부트가 미리 생성해놓은 객체를 가져다가 자동 연결
@@ -22,10 +24,13 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
 
+        log.info(form.toString());
+
         Article article = form.toEntity();
+        log.info(article.toString());
 
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity //DB가 해당 객체를 인식 가능!
+@AllArgsConstructor
+@ToString
 public class Article {
 
     @Id //대표값을 지정
@@ -19,18 +24,18 @@ public class Article {
     private String content;
 
 
-    public Article(Long id, String title, String content) {
+    /*public Article(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
-    }
+    }*/
 
-    @Override
+  /*  @Override
     public String toString() {
         return "Article{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
-    }
+    }*/
 }
