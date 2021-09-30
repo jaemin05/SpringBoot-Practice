40ab57e31bab3cd6d1a6fec56e304f661b89c141
son <sonjm3518@dsm.hs.kr>
2021-09-30 15:47:49 +0900

    ADMIN, MEMBER

---
+++ test/src/main/java/com/example/test/Service/Role.java
@@ -0,0 +1,13 @@
+package com.example.test.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Role {
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    private String value;
+}
