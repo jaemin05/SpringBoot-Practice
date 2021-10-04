70930a0e513084a005ec7f913087615fdb821219
son <sonjm3518@dsm.hs.kr>
2021-10-04 22:17:46 +0900

    Author's Model has PK and firstName and lastName fields.

---
+++ Library/src/main/java/com/example/library/model/Author.java
@@ -0,0 +1,20 @@
+package com.example.library.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
+
