3f02f43ef0cd4399996ea63f90166c4f75348001
son <sonjm3518@dsm.hs.kr>
2021-10-15 11:39:53 +0900

    Implement findByIsbn

---
+++ Library/src/main/java/com/example/library/Repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.library.Repository;
+
+import com.example.library.model.Book;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+    Optional<Book> findByIsbn(String isbn);
+}
