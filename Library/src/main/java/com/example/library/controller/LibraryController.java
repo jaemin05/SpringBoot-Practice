6d878e40793b243695fcfaf0ae768525800f2332
son <sonjm3518@dsm.hs.kr>
2021-10-19 10:21:17 +0900

    implement the controller

---
+++ Library/src/main/java/com/example/library/controller/LibraryController.java
@@ -0,0 +1,77 @@
+package com.example.library.controller;
+
+import com.example.library.Repository.BookRepository;
+import com.example.library.Repository.MemberRepository;
+import com.example.library.model.Author;
+import com.example.library.model.Book;
+import com.example.library.model.Member;
+import com.example.library.model.request.AuthorCreationRequest;
+import com.example.library.model.request.BookCreationRequest;
+import com.example.library.model.request.BookLendRequest;
+import com.example.library.model.request.MemberCreationRequest;
+import com.example.library.service.LibraryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
+@RequestMapping(value="/api/library")
+@RequiredArgsConstructor
+public class LibraryController {
+    private final LibraryService libraryService;
+
+    @GetMapping("/book")
+    public ResponseEntity readBooks(@RequestParam(required = false) String isbn) {
+        if(isbn == null) {
+            return ResponseEntity.ok(libraryService.readBooks());
+        }
+        return ResponseEntity.ok(libraryService.readBood(isbn));
+    }
+
+    @GetMapping("/book/{bookId}")
+    public ResponseEntity<Book> readBook (@PathVariable Long bookId) {
+        return ResponseEntity.ok(libraryService.readBook(bookId));
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity<Book> createBook (@RequestBody BookCreationRequest request) {
+        return ResponseEntity.ok(libraryService.createBook(request));
+    }
+
+    @DeleteMapping("/book/{bookId}")
+    public ResponseEntity<Void> deleteBook (@PathVariable Long bookId) {
+        libraryService.deleteBook(bookId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/member")
+    public ResponseEntity<Member> createMember (@RequestBody MemberCreationRequest request) {
+        return ResponseEntity.ok(libraryService.createMember((MemberRepository) request));
+    }
+
+    @PatchMapping("/member/{memberId}")
+    public ResponseEntity<Member> updateMember (@RequestBody MemberCreationRequest request, @PathVariable Long memberId) {
+        return ResponseEntity.ok(libraryService.updateMember(memberId, request ));
+    }
+
+    @PostMapping("/book/lend")
+    public ResponseEntity<List<String>> lendBook(@RequestBody BookLendRequest bookLendRequests) {
+        return ResponseEntity.ok(libraryService.lendABook(bookLendRequests));
+    }
+    @PostMapping("/author")
+    public ResponseEntity<Author> createAuthor (@RequestBody AuthorCreationRequest request) {
+        return ResponseEntity.ok(libraryService.createAuthor(request));
+    }
+
+
+
+
+
+
+    
+
+
+
+}
