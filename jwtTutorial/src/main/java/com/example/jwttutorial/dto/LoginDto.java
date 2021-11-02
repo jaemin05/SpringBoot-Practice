bc7798497c56a605a11daa1ea8fa2e9c019a4dd3
son <sonjm3518@dsm.hs.kr>
2021-11-03 00:06:48 +0900

    [Implement] username, password

---
+++ jwtTutorial/src/main/java/com/example/jwttutorial/dto/LoginDto.java
@@ -0,0 +1,24 @@
+package com.example.jwttutorial.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    @NotNull
+    @Size(min=3,max=50)
+    private String username;
+
+    @NotNull
+    @Size(min=3, max=100)
+    private String password;
+
+}
