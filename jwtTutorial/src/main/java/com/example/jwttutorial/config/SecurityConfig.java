7d7414f7a25f186321fe887cbb42f368df3ce88c
son <sonjm3518@dsm.hs.kr>
2021-11-03 00:15:08 +0900

    [Add] Receive jwtAuthenticationEntryPoint

---
+++ jwtTutorial/src/main/java/com/example/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.example.jwttutorial.config;
+
+import com.example.jwttutorial.jwt.TokenProvider;
+import com.example.jwttutorial.jwt.jwtAccessDeniedHandler;
+import com.example.jwttutorial.jwt.jwtAuthenticationEntryPoint;
+import com.example.jwttutorial.jwt.jwtSecurityConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final jwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final jwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(TokenProvider tokenProvider, jwtAccessDeniedHandler jwtAccessDeniedHandler, jwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                        ,"/error"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new jwtSecurityConfig((tokenProvider)));
+    }
+}
