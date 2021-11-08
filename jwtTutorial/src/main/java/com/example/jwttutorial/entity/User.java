1002d8b982a7f81b8aac28de454870f11a788518
son <sonjm3518@dsm.hs.kr>
2021-11-08 22:33:54 +0900

    [Add] @Builder

--- jwtTutorial/src/main/java/com/example/jwttutorial/entity/User.java
+++ jwtTutorial/src/main/java/com/example/jwttutorial/entity/User.java
@@ -1,10 +1,7 @@
 package com.example.jwttutorial.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 import javax.persistence.*;
@@ -14,6 +11,7 @@ import java.util.Set;
 @Table(name = "user ")
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
