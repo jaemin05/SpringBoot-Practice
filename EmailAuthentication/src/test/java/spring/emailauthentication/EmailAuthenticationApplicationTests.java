988e40c7306628ddfce18a4d7be9c1546d661621
son <sonjm3518@dsm.hs.kr>
2021-11-15 22:00:48 +0900

    init

---
+++ EmailAuthentication/src/test/java/spring/emailauthentication/EmailAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.emailauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
