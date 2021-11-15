92fcfd8fce520db35a66bc452680a899ddb441cd
son <sonjm3518@dsm.hs.kr>
2021-11-16 01:36:16 +0900

    [Implement] loadByEmail

---
+++ EmailAuthentication/src/main/java/spring/emailauthentication/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package spring.emailauthentication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import spring.emailauthentication.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> loadByEmail(String email);
+}
