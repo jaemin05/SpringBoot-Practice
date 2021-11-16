5c6dffeb8f304ecc8df04f7cb4fc083ad01e8f07
son <sonjm3518@dsm.hs.kr>
2021-11-16 22:04:43 +0900

    [Implement] doFilterInternal

---
+++ EmailAuthentication/src/main/java/spring/emailauthentication/security/JwtFilter.java
@@ -0,0 +1,35 @@
+package spring.emailauthentication.security;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.descriptor.web.ContextHandler;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    public JwtFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtProvider.resolveToken(request);
+        if(token!=null && jwtProvider.validationToken(token)) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
+}
