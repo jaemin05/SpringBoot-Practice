88d65247d3c20b6fe094fdcd72b4b72a5af2f894
son <sonjm3518@dsm.hs.kr>
2021-11-18 02:01:10 +0900

    [Implement] Cors

---
+++ EmailAuthentication/src/main/java/spring/LoginLogout/config/WebConfig.java
@@ -0,0 +1,20 @@
+package spring.LoginLogout.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Cors : 다른 출처의 자원을 공유할 수 있도록 설정하는 권한 체제
+@Configuration
+@EnableWebSecurity
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+        public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("X-AUTH-TOKEN")
+                .allowCredentials(true) //cookie
+                .allowedOrigins("http://localhost:3000");
+    }
+}
