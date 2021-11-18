d3d82c1fef68dc3cd7c3a2b59b9ba29925b0a5b3
son <sonjm3518@dsm.hs.kr>
2021-11-18 17:11:57 +0900

    [Add] DI

---
+++ EmailAuthentication/src/main/java/spring/LoginLogout/dto/UserDto.java
@@ -0,0 +1,30 @@
+package spring.LoginLogout.dto;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import spring.LoginLogout.domain.User;
+import spring.LoginLogout.enums.Gender;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String name;
+    private Gender gender;
+
+
+    public UserDto(User member) {
+            this.email = member.getEmail();
+            this.password = member.getPassword();
+            this.phoneNumber = member.getPhoneNumber();
+            this.name = member.getName();
+            this.gender = member.getGender();
+        }
+}
