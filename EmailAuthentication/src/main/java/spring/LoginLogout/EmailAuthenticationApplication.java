3e59280d4bc793b21f2fde4eeca37bc64d49614e
son <sonjm3518@dsm.hs.kr>
2021-11-18 17:12:14 +0900

    init

--- EmailAuthentication/src/main/java/spring/LoginLogout/EmailAuthenticationApplication.java
+++ EmailAuthentication/src/main/java/spring/LoginLogout/EmailAuthenticationApplication.java
@@ -1,4 +1,4 @@
-package spring.emailauthentication;
+package spring.LoginLogout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
