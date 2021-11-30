6f514db8ceb72751e5e67ba964b354ba3ec526c4
son <sonjm3518@dsm.hs.kr>
2021-11-30 20:29:39 +0900

    [Implement] email

---
+++ EmailAuthentication/src/main/java/payload/EmailRequest.java
@@ -0,0 +1,13 @@
+package payload;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+
+@Getter
+@NoArgsConstructor
+public class EmailRequest {
+    @Email
+    private String email;
+}
