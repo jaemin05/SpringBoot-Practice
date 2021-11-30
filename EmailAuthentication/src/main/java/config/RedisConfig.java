1d16dd5a980364250ab8a69ea8d4fb181244cc75
son <sonjm3518@dsm.hs.kr>
2021-11-30 19:55:18 +0900

    [Implement] RedisConfig

---
+++ EmailAuthentication/src/main/java/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig{
+
+    @Value("${spring.redis.host}")
+    private String REDIS_HOST;
+
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisSentinelConfiguration = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        return new LettuceConnectionFactory(redisSentinelConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisProperties() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
