eb6d1651a1863175b4b89a5119aeca36fcd2b091
son <sonjm3518@dsm.hs.kr>
2021-11-30 20:28:34 +0900

    [Implement] certification entity

---
+++ EmailAuthentication/src/main/java/Entity/Certification.java
@@ -0,0 +1,41 @@
+package Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.io.Serializable;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash
+public class Certification implements Serializable {
+    @Id
+    @Indexed
+    private String email;
+
+    private String code;
+    private Certified cerified;
+
+    public Certification updateCretified(Certified cerified) {
+        this.cerified = cerified;
+        return this;
+    }
+
+    public Certification updateCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+
+
+
+
+}
