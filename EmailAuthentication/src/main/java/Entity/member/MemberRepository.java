ce4237e7da13035e6f68980bfc8b8de530251419
son <sonjm3518@dsm.hs.kr>
2021-12-05 23:34:01 +0900

    [Implement] findByEmail

---
+++ EmailAuthentication/src/main/java/Entity/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package Entity.member;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
