61d6c1d9536c6ac40dfd6af6362271d5f607da89
son <sonjm3518@dsm.hs.kr>
2021-12-05 23:37:17 +0900

    [Add] signup

--- EmailAuthentication/src/main/java/service/member/MemberService.java
+++ EmailAuthentication/src/main/java/service/member/MemberService.java
@@ -2,8 +2,10 @@ package service.member;
 
 import payload.EmailRequest;
 import payload.EmailVerifiedRequest;
+import payload.MemberRequest;
 
 public interface MemberService {
     void sendEmail(EmailRequest request);
     void verifyAccount(EmailVerifiedRequest request);
+    void signup(MemberRequest request);
 }
