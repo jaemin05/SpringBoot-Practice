86726a7e79b82e7494fc83e98a83e36a5677f589
son <sonjm3518@dsm.hs.kr>
2021-12-06 20:13:56 +0900

    [Implement] CodeAlreadyExpiredException

--- EmailAuthentication/src/main/java/com/example/practice/exception/CodeAlreadyExpiredException.java
+++ EmailAuthentication/src/main/java/com/example/practice/exception/CodeAlreadyExpiredException.java
@@ -4,7 +4,11 @@ import com.example.practice.error.ErrorCode;
 import com.example.practice.error.Exception.BusinessException;
 
 public class CodeAlreadyExpiredException extends BusinessException {
-    public CodeAlreadyExpiredException() {
+
+    public static BusinessException EXCEPTION =
+            new CodeAlreadyExpiredException();
+
+    private CodeAlreadyExpiredException() {
         super(ErrorCode.CODE_ALREADY_EXPIRED);
     }
 }
