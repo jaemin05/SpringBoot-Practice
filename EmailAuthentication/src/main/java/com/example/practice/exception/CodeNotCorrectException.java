57001abee4e0c51a4d342aa02281c1ba922f71e6
son <sonjm3518@dsm.hs.kr>
2021-12-06 20:14:14 +0900

    [Implement] CodeNotCorrectException

--- EmailAuthentication/src/main/java/com/example/practice/exception/CodeNotCorrectException.java
+++ EmailAuthentication/src/main/java/com/example/practice/exception/CodeNotCorrectException.java
@@ -4,7 +4,11 @@ import com.example.practice.error.ErrorCode;
 import com.example.practice.error.Exception.BusinessException;
 
 public class CodeNotCorrectException extends BusinessException {
-    public CodeNotCorrectException() {
+
+    public static BusinessException EXCEPTION =
+            new CodeNotCorrectException();
+
+    private CodeNotCorrectException() {
         super(ErrorCode.CODE_NOT_CORRECT);
     }
 }
