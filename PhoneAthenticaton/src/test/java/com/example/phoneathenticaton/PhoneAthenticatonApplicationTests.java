69c874452c96e1a8c30e17bafd1ba0003bfd00e2
son <sonjm3518@dsm.hs.kr>
2021-12-06 21:30:12 +0900

    setting

---
+++ PhoneAthenticaton/src/test/java/com/example/phoneathenticaton/PhoneAthenticatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phoneathenticaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneAthenticatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
