88484bf52ef0c403d5374b8fbc9d511056088043
son <sonjm3518@dsm.hs.kr>
2021-12-07 22:52:17 +0900

    [Implement] BusinessException

---
+++ fileUpload/src/main/java/com/example/fileupload/error/Exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.example.fileupload.error.Exception;
+
+import com.example.fileupload.error.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+}
