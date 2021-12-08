0ad3d6ae5b89b4f47aebdc68735b95442d222f36
son <sonjm3518@dsm.hs.kr>
2021-12-08 13:42:02 +0900

    [Add] File Not Set Size

--- fileUpload/src/main/java/com/example/fileupload/error/ErrorCode.java
+++ fileUpload/src/main/java/com/example/fileupload/error/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
     File_NOT_FOUND(500, "File Not Found"),
     FILE_EMPTY(409, "File is Empty"),
-    FILE_NOT_STORE(500, "File Not Store");
+    FILE_NOT_STORE(500, "File Not Store"),
+    FILE_NOT_SET_SIZE(500, "File Not Set Size");
 
     private int statues;
     private String message;
