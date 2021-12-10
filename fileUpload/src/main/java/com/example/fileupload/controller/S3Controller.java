ac7abb337dc5fab6569302a00a13be0f9b82e014
son <sonjm3518@dsm.hs.kr>
2021-12-10 13:15:23 +0900

    [Add] upload

--- fileUpload/src/main/java/com/example/fileupload/controller/S3Controller.java
+++ fileUpload/src/main/java/com/example/fileupload/controller/S3Controller.java
@@ -5,14 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RequiredArgsConstructor
 @RestController
 public class S3Controller {
     private final S3Uploader s3Uploader;
 
-    @PostMapping("/api/v1/upload")
-    public String uploadImage(@RequestParam MultipartFile file) {
-        return s3Uploader.uploadImage(file);
+    @PostMapping("/images")
+    public String images(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.uploadImages(multipartFile, "static");
     }
 
 }
