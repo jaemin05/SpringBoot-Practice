a31fa5b928ed263edd57740ae41f44d57c87de44
son <sonjm3518@dsm.hs.kr>
2021-12-16 00:50:44 +0900

    setting

---
+++ src/main/java/com/example/socketio/SocketIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socketio/SocketIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
