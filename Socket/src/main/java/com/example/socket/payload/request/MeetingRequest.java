cb9531506196273d79074d9c25c5e63df8e50df4
son <sonjm3518@dsm.hs.kr>
2021-12-20 09:52:17 +0900

    [Implement]  Meeting

---
+++ Socket/src/main/java/com/example/socket/payload/request/MeetingRequest.java
@@ -0,0 +1,15 @@
+package com.example.socket.payload.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MeetingRequest {
+        private String name;
+        private RoomType roomType;
+        private String admin;
+        private String info;
+}
