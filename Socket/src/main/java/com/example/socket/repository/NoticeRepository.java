6dd0f35fbb4de03810bca0f955219888070cc538
son <sonjm3518@dsm.hs.kr>
2021-12-21 14:55:45 +0900

    [Implement] findAllByRoomAndTargetDateAfter

---
+++ Socket/src/main/java/com/example/socket/repository/NoticeRepository.java
@@ -0,0 +1,11 @@
+package com.example.socket.repository;
+
+import com.example.socket.entity.chat.Notice;
+import com.example.socket.entity.chat.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> findALlByRoomAndTargetDateAfter(Room room, LocalDate date);
+}
