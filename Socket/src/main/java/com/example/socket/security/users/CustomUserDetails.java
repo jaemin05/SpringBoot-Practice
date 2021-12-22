da1966b57d853bc0d1315ebb05a53cb99b5022ba
son <sonjm3518@dsm.hs.kr>
2021-12-22 17:08:18 +0900

    [Implement] UserDetails

---
+++ Socket/src/main/java/com/example/socket/security/users/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.socket.security.users;
+
+import com.example.socket.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        GrantedAuthority grantedAuthority = () -> member.getRole().name();
+        //.singleton: 단 한개의 객체만 저장 가능한 컬렉션을 만들고 싶을 때 사용한다.
+        return Collections.singleton(grantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getName();
+    }
+
+    public String getUserId() {
+        return member.getId().toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
