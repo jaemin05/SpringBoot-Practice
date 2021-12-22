febacacb6d1e8cebb47fc1eeddac7ac1820f292a
son <sonjm3518@dsm.hs.kr>
2021-12-22 21:55:33 +0900

    [Fix] Exception

--- Socket/src/main/java/com/example/socket/security/users/CustomUserDetailsService.java
+++ Socket/src/main/java/com/example/socket/security/users/CustomUserDetailsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
-    
+
     @Override
     public CustomUserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
         return memberRepository.findById(id)
