e9350e11b1af372414cfed7953f476004eb183c0
son <sonjm3518@dsm.hs.kr>
2021-12-23 16:06:12 +0900

    [Implement] SocketServiceImpl

---
+++ Socket/src/main/java/com/example/socket/service/SocketServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.socket.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SocketServiceImpl {
+}
