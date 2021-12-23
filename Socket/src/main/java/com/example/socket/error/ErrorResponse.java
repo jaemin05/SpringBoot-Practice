500ba3fb4826f122f3411a1e0ef603bd84e1261e
son <sonjm3518@dsm.hs.kr>
2021-12-23 20:20:38 +0900

    [Add] status

--- Socket/src/main/java/com/example/socket/error/ErrorResponse.java
+++ Socket/src/main/java/com/example/socket/error/ErrorResponse.java
@@ -1,10 +1,13 @@
 package com.example.socket.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ErrorResponse {
     private String message;
+    private int status;
 }
