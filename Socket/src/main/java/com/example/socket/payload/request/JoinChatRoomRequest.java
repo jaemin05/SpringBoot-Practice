9e63636e87a8a44ebd8eb3c4e4bc3cf53ac3c031
son <sonjm3518@dsm.hs.kr>
2021-12-28 20:15:45 +0900

    [Delete] @NoArgsConstructor

--- Socket/src/main/java/com/example/socket/payload/request/JoinChatRoomRequest.java
+++ Socket/src/main/java/com/example/socket/payload/request/JoinChatRoomRequest.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class JoinChatRoomRequest {
     private String roomId;
     private String chatCategory;
