e9b806314a2f477781d98085508bebd61b33b5e6
son <sonjm3518@dsm.hs.kr>
2021-12-28 20:22:23 +0900

    [Fix] isAccountNonLocked

--- Socket/src/main/java/com/example/socket/security/users/CustomUserDetails.java
+++ Socket/src/main/java/com/example/socket/security/users/CustomUserDetails.java
@@ -40,7 +40,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
