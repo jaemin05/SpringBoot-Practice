4cd9005b256484b39370cb48d83159dd0e17659c
son <sonjm3518@dsm.hs.kr>
2022-01-01 21:27:58 +0900

    [Add] Dependency Injection

--- OAuth/src/main/java/com/example/oauth/api/entity/user/User.java
+++ OAuth/src/main/java/com/example/oauth/api/entity/user/User.java
@@ -1,11 +1,15 @@
 package com.example.oauth.api.entity.user;
 
+import com.example.oauth.oauth.entity.ProviderType;
+import com.example.oauth.oauth.entity.RoleType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.apache.tomcat.jni.Local;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -45,5 +49,43 @@ public class User {
     @Size(max = 512)
     private String profileImageUrl;
 
+    @Column(name = "PROVIDER_TYPE",length = 20)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ProviderType providerType;
 
+    @Column(name = "ROLE_TYPE", length = 20)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private RoleType roleType;
+
+    @Column(name = "CREATE_AT")
+    @NotNull
+    private LocalDateTime createAt;
+
+    @Column(name = "MODIFIED_AT")
+    @NotNull
+    private LocalDateTime modifiedAt;
+
+    public User(
+            @NotNull @Size(max = 100) String username,
+            @NotNull @Size(max = 128) String password,
+            @NotNull @Size(max = 512) String email,
+            @NotNull boolean emailVerified,
+            @NotNull @Size(max = 512) String profileImageUrl,
+            @NotNull ProviderType providerType,
+            @NotNull RoleType roleType,
+            @NotNull LocalDateTime createAt,
+            @NotNull LocalDateTime modifiedAt
+            ) {
+        this.username = username;
+        this.password = password != null ? password : "NO_PASSWORD";
+        this.email = email != null ? email : "NO_EMAIL";
+        this.emailVerified = emailVerified;
+        this.profileImageUrl = profileImageUrl != null ? profileImageUrl : "";
+        this.providerType = providerType;
+        this.roleType = roleType;
+        this.createAt = createAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
