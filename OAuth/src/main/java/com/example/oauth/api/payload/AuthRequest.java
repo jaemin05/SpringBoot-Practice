7971af7794d737f65c050a915458fda4fbffeaa9
son <sonjm3518@dsm.hs.kr>
2022-01-01 21:08:12 +0900

    [Implement] AuthRequest

---
+++ OAuth/src/main/java/com/example/oauth/api/payload/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.example.oauth.api.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String id;
+    private String password;
+}
