a67321d6b57348207d1755c661fca48efd38b8e6
son <sonjm3518@dsm.hs.kr>
2022-01-03 22:37:22 +0900

    📄 :: WebMvcConfig

--- OAuth/src/main/java/com/example/oauth/config/WebConfig.java
+++ OAuth/src/main/java/com/example/oauth/config/WebConfig.java
@@ -1,6 +1,12 @@
 package com.example.oauth.config;
 
+import lombok.Builder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,13 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
     private final long MAX_AGE = 3600;
 
     @Override
-    public void addCorsMapping(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry){
         registry
-                .addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
+                .addMapping("/**") //CORS 적용할 URL 패턴
+                .allowedOrigins("*") // 자원을 공유할 오리진 지정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") //요청 허용 메서드
+                .allowedHeaders("*") //요청 허용 헤더
+                .allowCredentials(true) //쿠키허용
                 .maxAge(MAX_AGE);
     }
 }
