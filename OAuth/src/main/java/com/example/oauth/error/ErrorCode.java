9a7129687ececd070824d1127e79b5bfc4b35793
son <sonjm3518@dsm.hs.kr>
2022-01-05 23:37:06 +0900

    ⚡:: 에러 코드 추가

--- OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
+++ OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    PROVIDER_TYPE_INVALID(409, "Provider Type Invalid"),
+    USER_NOT_FOUND(404, "User Not Found");
+
     private final int status;
     private final String message;
 }
