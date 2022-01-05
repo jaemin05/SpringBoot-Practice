89fea324661c3f98efea0d69176386b771985ad1
son <sonjm3518@dsm.hs.kr>
2022-01-05 23:40:40 +0900

    📑 :: OAuth 유저

---
+++ OAuth/src/main/java/com/example/oauth/security/OAuth/user/OAuthUserInfo.java
@@ -0,0 +1,25 @@
+package com.example.oauth.security.OAuth.user;
+
+import java.util.Map;
+
+//abstract: 추상
+//추상적으로 정보를 모아두고 구체적으로는 페이스북, 네이버, 구글 등으로 따로 만듬
+public abstract class OAuthUserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuthUserInfo(Map<String, Object> attributes ){
+        this.attributes = attributes;
+    }
+
+    public Map<String, Object> getAttributes(){
+        return attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getName();
+
+    public abstract String getEmail();
+
+    public abstract String getImageUrl();
+}
