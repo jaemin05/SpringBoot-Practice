9f5ed2199f911a0510b3d8159829eec1585f2e7b
son <sonjm3518@dsm.hs.kr>
2022-01-06 18:07:33 +0900

    ❌ :: 삭제

--- OAuth/src/main/java/com/example/oauth/config/SecurityConfig.java
+++ OAuth/src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.oauth.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,21 +11,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .csrf().disable()
-                .formLogin().disable()
-
-                .authorizeRequests()
-                //관리자만 들어갈 수 있는 페이지 정하기!!!
-                .antMatchers("/**").permitAll();
-    }
+    public
 }
