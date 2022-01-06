2886c9a6b1b86c313c31f24f7cf287732018af96
son <sonjm3518@dsm.hs.kr>
2022-01-06 22:51:19 +0900

    📄 :: 권한 정보가 없는 토큰 오류

--- OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
+++ OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     PROVIDER_TYPE_INVALID(409, "Provider Type Invalid"),
-    USER_NOT_FOUND(404, "User Not Found");
+    USER_NOT_FOUND(404, "User Not Found"),
+    TOKEN_NOT_AUTHORITIES(403, "Token Not Authorities");
 
     private final int status;
     private final String message;
