14221e8688c5c19c8c9683daf2e2159e9d6dc6b9
son <sonjm3518@dsm.hs.kr>
2022-01-07 19:07:51 +0900

    ⚡ :: Refresh Token Not Found 에러 추가

--- OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
+++ OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
     PROVIDER_TYPE_INVALID(409, "Provider Type Invalid"),
     USER_NOT_FOUND(404, "User Not Found"),
-    TOKEN_NOT_AUTHORITIES(403, "Token Not Authorities");
+    TOKEN_NOT_AUTHORITIES(403, "Token Not Authorities"),
+    URI_NOT_AUTHORIZED(403, "Uri Not Authorized"),
+    REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found");
 
     private final int status;
     private final String message;
