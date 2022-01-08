ef0e671d3dacc3eaf8d6b25917cf73106bb6f10b
son <sonjm3518@dsm.hs.kr>
2022-01-08 23:37:50 +0900

    🎨 :: 필요 없는 것들 삭제

--- OAuth/src/main/java/com/example/oauth/config/JwtConfig.java
+++ OAuth/src/main/java/com/example/oauth/config/JwtConfig.java
@@ -1,6 +1,5 @@
 package com.example.oauth.config;
 
-import com.example.oauth.security.token.AuthToken;
 import com.example.oauth.security.token.TokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
