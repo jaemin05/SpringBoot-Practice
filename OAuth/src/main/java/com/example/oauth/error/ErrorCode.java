b2250f4c8b7f83cbeda012087bb79bf53977c95c
son <sonjm3518@dsm.hs.kr>
2022-01-09 17:53:11 +0900

    ⚡ :: 이메일 존재 여부 에러 추가

--- OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
+++ OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "User Not Found"),
     TOKEN_NOT_AUTHORITIES(403, "Token Not Authorities"),
     URI_NOT_AUTHORIZED(403, "Uri Not Authorized"),
-    REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found");
+    REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found"),
+    EMAIL_ALREADY_EXISTS(409, "Email Already Exists");
 
     private final int status;
     private final String message;
