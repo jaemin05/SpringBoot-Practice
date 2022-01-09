a1b6aa3ba48f1587b93c12850a5ea2959eca05bf
son <sonjm3518@dsm.hs.kr>
2022-01-09 17:54:38 +0900

    📂 :: 파일 이름 변경

--- OAuth/src/main/java/com/example/oauth/exception/UserNotFoundException.java
+++ OAuth/src/main/java/com/example/oauth/exception/UserNotFoundException.java
@@ -3,11 +3,11 @@ package com.example.oauth.exception;
 import com.example.oauth.error.ErrorCode;
 import com.example.oauth.error.Exception.BusinessException;
 
-public class UserNotFound extends BusinessException {
+public class UserNotFoundException extends BusinessException {
     public static BusinessException Exception =
-            new UserNotFound();
+            new UserNotFoundException();
 
-    private UserNotFound(){
+    private UserNotFoundException(){
         super(ErrorCode.USER_NOT_FOUND);
     }
 }
