4a08462bf8f4dfb488b6ab3bd69dfd0cc131d066
son <sonjm3518@dsm.hs.kr>
2022-01-10 01:02:32 +0900

    ⚡ :: 에러 추가

--- OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
+++ OAuth/src/main/java/com/example/oauth/error/ErrorCode.java
@@ -11,7 +11,11 @@ public enum ErrorCode {
     TOKEN_NOT_AUTHORITIES(403, "Token Not Authorities"),
     URI_NOT_AUTHORIZED(403, "Uri Not Authorized"),
     REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found"),
-    EMAIL_ALREADY_EXISTS(409, "Email Already Exists");
+    EMAIL_ALREADY_EXISTS(409, "Email Already Exists"),
+    INVALID_ACCESS_TOKEN(409, "Invalid Access Token"),
+    INVALID_REFRESH_TOKEN(409, "Invalid Refresh Token"),
+    NOT_EXPIRED_TOKEN_YET(500, "Not Expired Token Yet"),
+    USER_LOGOUT(500, "User Logout");
 
     private final int status;
     private final String message;
