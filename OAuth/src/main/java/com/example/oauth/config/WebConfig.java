c8c2bc4487629a20f4e30a8c32fec159deca16b1
son <sonjm3518@dsm.hs.kr>
2022-01-12 10:42:12 +0900

    ⚡ :: cors 설정

--- OAuth/src/main/java/com/example/oauth/config/WebConfig.java
+++ OAuth/src/main/java/com/example/oauth/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.oauth.config;
 
 import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private final long MAX_AGE = 3600;
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
@@ -20,8 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .addMapping("/**") //CORS 적용할 URL 패턴
                 .allowedOrigins("*") // 자원을 공유할 오리진 지정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") //요청 허용 메서드
-                .allowedHeaders("*") //요청 허용 헤더
-                .allowCredentials(true) //쿠키허용
-                .maxAge(MAX_AGE);
+                .allowedHeaders("*"); //요청 허용 헤더
     }
 }
