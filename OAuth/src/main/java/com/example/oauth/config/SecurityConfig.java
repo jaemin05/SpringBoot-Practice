367f77fc6a0516d93996ddb8e9e468c2f4ad4afc
son <sonjm3518@dsm.hs.kr>
2022-01-12 10:42:01 +0900

    ⚡ :: security 설정

--- OAuth/src/main/java/com/example/oauth/config/SecurityConfig.java
+++ OAuth/src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -1,41 +1,27 @@
 package com.example.oauth.config;
 
 import com.example.oauth.domain.user.Role;
-import com.example.oauth.domain.user.UserRefreshTokenRepository;
-import com.example.oauth.security.oauth.handler.OAuthAuthenticationFailureHandler;
-import com.example.oauth.security.oauth.handler.OAuthAuthenticationSuccessHandler;
-import com.example.oauth.security.repository.HttpCookieOAuthAuthorizationRequestRepository;
 import com.example.oauth.security.token.TokenAuthenticationFilter;
 import com.example.oauth.security.token.TokenProvider;
-import com.example.oauth.service.CustomOAuthUserService;
+import com.example.oauth.service.CustomOAuth2UserService;
 import com.example.oauth.service.CustomUserDetailsService;
-import com.fasterxml.jackson.core.filter.TokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        securedEnabled = true,
-        jsr250Enabled = true,
-        prePostEnabled = true
-)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomUserDetailsService customUserDetailsService;
-    private final CustomOAuthUserService customOAuthUserService;
+    private final CustomOAuth2UserService customOAuthUserService;
     private final TokenProvider tokenProvider;
-    private final UserRefreshTokenRepository userRefreshTokenRepository;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
@@ -63,16 +49,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizationEndpoint()
                     //클라이언트 처음 로그인 URI
                     .baseUri("/oauth2/authorization")
-                    .authorizationRequestRepository(httpCookieOAuthAuthorizationRequestRepository())
                 .and()
                     .redirectionEndpoint()
                     .baseUri("/*/oauth2/code/*")
                 .and()
                     .userInfoEndpoint()
-                    .userService(customOAuthUserService)
-                .and()
-                    .successHandler(oAuthAuthenticationSuccessHandler())
-                    .failureHandler(oAuthAuthenticationFailureHandler());
+                    .userService(customOAuthUserService);
     }
 
     @Override
@@ -90,28 +72,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public TokenAuthenticationFilter tokenAuthenticationFilter(){
         return new TokenAuthenticationFilter(tokenProvider);
     }
-
-    @Bean
-    public HttpCookieOAuthAuthorizationRequestRepository httpCookieOAuthAuthorizationRequestRepository(){
-        return new HttpCookieOAuthAuthorizationRequestRepository();
-    }
-
-    @Bean
-    public OAuthAuthenticationSuccessHandler oAuthAuthenticationSuccessHandler() {
-        return new OAuthAuthenticationSuccessHandler(
-                userRefreshTokenRepository,
-                httpCookieOAuthAuthorizationRequestRepository()
-        );
-    }
-
-    @Bean
-    public OAuthAuthenticationFailureHandler oAuthAuthenticationFailureHandler(){
-        return new OAuthAuthenticationFailureHandler(httpCookieOAuthAuthorizationRequestRepository());
-    }
-
-
-
-
-
-
 }
