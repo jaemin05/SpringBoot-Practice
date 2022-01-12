3988c1db6d5a9d5d3bfcf9d9a83776f858b4df86
son <sonjm3518@dsm.hs.kr>
2022-01-12 10:44:44 +0900

    ⚡ :: @ConfigurationPropertiesScan 추가

--- OAuth/src/main/java/com/example/oauth/OAuthApplication.java
+++ OAuth/src/main/java/com/example/oauth/OAuthApplication.java
@@ -1,12 +1,13 @@
 package com.example.oauth;
 
-import com.example.oauth.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
 public class OAuthApplication {
 
     public static void main(String[] args) {
