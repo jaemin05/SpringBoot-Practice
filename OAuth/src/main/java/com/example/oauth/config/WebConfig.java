344d3421109a93953b7a29a9c31ad67f4ae5570d
son <sonjm3518@dsm.hs.kr>
2022-01-15 00:19:13 +0900

    📂 :: 코드 정리

--- OAuth/src/main/java/com/example/oauth/config/WebConfig.java
+++ OAuth/src/main/java/com/example/oauth/config/WebConfig.java
@@ -1,13 +1,7 @@
 package com.example.oauth.config;
 
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
