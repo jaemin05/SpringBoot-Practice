db70d766112716b2408d4d898d1110e08971d64d
son <sonjm3518@dsm.hs.kr>
2022-01-15 00:53:28 +0900

    📄 :: 게스트 Controller

---
+++ OAuth/src/main/java/com/example/oauth/controller/auth/GuestController.java
@@ -0,0 +1,34 @@
+package com.example.oauth.controller.auth;
+
+import com.example.oauth.dto.Request.LoginRequest;
+import com.example.oauth.dto.Request.SignUpRequest;
+import com.example.oauth.dto.Request.TokenRequest;
+import com.example.oauth.dto.response.TokenResponse;
+import com.example.oauth.service.GuestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/guest")
+public class GuestController {
+    private final GuestService guestService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody @Valid SignUpRequest signUpRequest){
+        return ResponseEntity.ok(guestService.signup(signUpRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenRequest> login(@RequestBody @Valid LoginRequest loginRequest){
+        return ResponseEntity.ok(guestService.login(loginRequest));
+    }
+
+    @PostMapping("/reissue")
+    public TokenResponse reissue(@RequestHeader("X-Refresh-Token") String refresh){
+        return guestService.reissue(refresh);
+    }
+}
