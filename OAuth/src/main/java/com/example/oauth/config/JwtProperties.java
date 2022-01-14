1a87948d088b72565a9f0bda848c209c5d6459ab
son <sonjm3518@dsm.hs.kr>
2022-01-15 00:17:54 +0900

    ⚡ :: @ConstructorBinding 추가

--- OAuth/src/main/java/com/example/oauth/config/JwtProperties.java
+++ OAuth/src/main/java/com/example/oauth/config/JwtProperties.java
@@ -4,21 +4,20 @@ import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
 
 @Getter
 @ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    private final String secretKey;
+    private final String secretToken;
     private final long accessTokenExp;
     private final long refreshTokenExp;
 
-    public JwtProperties(String secretKey, long accessTokenExp, long refreshTokenExp){
-        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+    public JwtProperties(String secretToken, long accessTokenExp, long refreshTokenExp){
+        this.secretToken = Base64.getEncoder().encodeToString(secretToken.getBytes(StandardCharsets.UTF_8));
         this.accessTokenExp = accessTokenExp;
         this.refreshTokenExp = refreshTokenExp;
     }
