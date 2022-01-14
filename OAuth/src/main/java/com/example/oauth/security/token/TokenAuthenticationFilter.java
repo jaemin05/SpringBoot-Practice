ef7f5f967e4d0ed6fa61da9ebee638c3bd68eb1d
son <sonjm3518@dsm.hs.kr>
2022-01-15 00:51:27 +0900

    ⚡ :: log 삭제

--- OAuth/src/main/java/com/example/oauth/security/token/TokenAuthenticationFilter.java
+++ OAuth/src/main/java/com/example/oauth/security/token/TokenAuthenticationFilter.java
@@ -1,11 +1,8 @@
 package com.example.oauth.security.token;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.HeaderUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
