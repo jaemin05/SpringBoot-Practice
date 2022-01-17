977723bd70d495d9709b40856a36690218b34639
son <sonjm3518@dsm.hs.kr>
2022-01-17 15:50:18 +0900

    📄 :: 유저 정보 확인

--- OAuth/src/main/java/com/example/oauth/controller/user/UserController.java
+++ OAuth/src/main/java/com/example/oauth/controller/user/UserController.java
@@ -1,4 +1,21 @@
 package com.example.oauth.controller.user;
 
+import com.example.oauth.dto.response.UserResponse;
+import com.example.oauth.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<UserResponse> getUser(){
+        return ResponseEntity.ok(userService.getUser());
+    }
 }
