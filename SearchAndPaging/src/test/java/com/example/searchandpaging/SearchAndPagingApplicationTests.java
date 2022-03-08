0e9dcd8d8211ea36e1bdf1a22184b8b3ca6a4e70
son <sonjm3518@dsm.hs.kr>
2022-03-08 19:50:12 +0900

    ⚙ :: basic code

---
+++ SearchAndPaging/src/test/java/com/example/searchandpaging/SearchAndPagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchandpaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchAndPagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
