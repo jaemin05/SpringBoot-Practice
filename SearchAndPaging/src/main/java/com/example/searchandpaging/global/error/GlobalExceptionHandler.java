1b4c35243be178e5faa6eca51dd30d0da24d87b7
son <sonjm3518@dsm.hs.kr>
2022-03-10 15:41:36 +0900

    📑 :: GlobalExceptionHandler 생성

---
+++ SearchAndPaging/src/main/java/com/example/searchandpaging/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.searchandpaging.global.error;
+
+import com.example.searchandpaging.global.error.exception.CustomException;
+import com.example.searchandpaging.global.error.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleGlobal(CustomException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(
+                ErrorResponse.builder()
+                        .status(errorCode.getStatus())
+                        .message(errorCode.getMessage())
+                        .build(),
+                HttpStatus.valueOf(errorCode.getStatus())
+        );
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<?> bindException(BindException e) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        for (FieldError error : e.getFieldErrors()) {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+}
