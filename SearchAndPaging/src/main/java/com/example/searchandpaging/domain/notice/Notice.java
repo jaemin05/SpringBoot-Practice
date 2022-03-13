f14f4d67ace9f09aac6947d0edd76a6626fde497
son <sonjm3518@dsm.hs.kr>
2022-03-13 21:44:57 +0900

    📑 :: Notice Entity

---
+++ SearchAndPaging/src/main/java/com/example/searchandpaging/domain/notice/Notice.java
@@ -0,0 +1,34 @@
+package com.example.searchandpaging.domain.notice;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @Column(length = 200)
+    private String content;
+
+    @Builder
+    public Notice(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
