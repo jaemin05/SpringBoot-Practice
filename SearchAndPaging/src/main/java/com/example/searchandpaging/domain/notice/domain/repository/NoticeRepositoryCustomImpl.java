9a5f88fc75d767e492aac15b97b25f09286f036a
son <sonjm3518@dsm.hs.kr>
2022-03-14 20:03:49 +0900

    📑 :: Notice Repository Custom Implement

---
+++ SearchAndPaging/src/main/java/com/example/searchandpaging/domain/notice/domain/repository/NoticeRepositoryCustomImpl.java
@@ -0,0 +1,35 @@
+package com.example.searchandpaging.domain.notice.domain.repository;
+
+import com.example.searchandpaging.domain.notice.domain.QNotice;
+import com.example.searchandpaging.domain.notice.exception.NoticeNotFoundException;
+import com.example.searchandpaging.domain.notice.presentation.dto.response.QQueryNoticeResponse_NoticeResponse;
+import com.example.searchandpaging.domain.notice.presentation.dto.response.QueryNoticeResponse.NoticeResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityListeners;
+import java.util.List;
+
+import static com.example.searchandpaging.domain.notice.domain.QNotice.notice;
+
+@RequiredArgsConstructor
+public class NoticeRepositoryCustomImpl implements NoticeRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<NoticeResponse> searchNoticeList(String keyword, Pageable pageable) {
+        return jpaQueryFactory
+                .select(new QQueryNoticeResponse_NoticeResponse(
+                    notice.title,
+                    notice.content,
+                    notice.createAt
+                ))
+                .from(notice)
+                .where(notice.title.contains(keyword))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+}
