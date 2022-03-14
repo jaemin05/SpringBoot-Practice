f6ae12d9e7c0421133ac8ba55550413b98235217
son <sonjm3518@dsm.hs.kr>
2022-03-14 20:12:05 +0900

    ⚡ :: Notice Not Found 추가

--- SearchAndPaging/src/main/java/com/example/searchandpaging/global/error/exception/ErrorCode.java
+++ SearchAndPaging/src/main/java/com/example/searchandpaging/global/error/exception/ErrorCode.java
@@ -1,17 +1,19 @@
 package com.example.searchandpaging.global.error.exception;
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-//@JsonFormat: JSON 응답값의 형식을 지정할 때 사용한다.
-//리턴받는 기댓값은 객체 형태이기 때문에 Shape.OBJECT로 설정
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
+    NOTICE_NOT_FOUND(404, "Notice-404-1", "Notice Not Found"),
+
+    FORBIDDEN(403, "COMMON-403-1", "Forbidden");
+
     private final int status;
+    private final String code;
     private final String message;
 }
