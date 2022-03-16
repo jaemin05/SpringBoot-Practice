4169cf8868d1671736e221681502075a5cd87c5b
son <sonjm3518@dsm.hs.kr>
2022-03-16 20:05:31 +0900

    📑 :: Notification Entity

---
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/notification/domain/Notification.java
@@ -0,0 +1,38 @@
+package com.example.fcmpractice.domain.notification.domain;
+
+import com.example.fcmpractice.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(length = 200)
+    private String content;
+
+    @Builder
+    public Notification(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
