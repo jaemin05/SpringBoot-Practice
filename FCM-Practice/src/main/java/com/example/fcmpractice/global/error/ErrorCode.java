c141265440566525e2f14d7c5d769c7cdca90990
son <sonjm3518@dsm.hs.kr>
2022-03-21 23:47:26 +0900

    ⚡ :: Password Not Correct Exception 추가

--- FCM-Practice/src/main/java/com/example/fcmpractice/global/error/ErrorCode.java
+++ FCM-Practice/src/main/java/com/example/fcmpractice/global/error/ErrorCode.java
@@ -9,7 +9,16 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    INVALID_PARAMETER(400, "PARAMETER-400-1", "Invalid Parameter");
+    INVALID_PARAMETER(400, "PARAMETER-400-1", "Invalid Parameter"),
+    PASSWORD_NOT_CORRECT(400, "AUTH-400-1", "Password Not Correct"),
+
+    EXPIRED_JWT(401, "JWT-401-1", "Expired Jwt"),
+    INVALID_JWT(401, "JWT-401-2", "Invalid Jwt"),
+
+    USER_NOT_FOUND(404,"USER-404-1", "User NotFound"),
+    JWT_NOT_FOUND(404, "JWT-404-3", "Jwt Not Found"),
+
+    ACCOUNT_ID_ALREADY_EXISTS(409, "USER-409-1", "AccountId Already Exists");
 
     private final int status;
     private final String code;
