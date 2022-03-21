436be087ca13dd73a97f8e2288613133cfecdb16
son <sonjm3518@dsm.hs.kr>
2022-03-21 23:45:41 +0900

    📑 :: User Facade

---
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/user/facade/UserFacade.java
@@ -0,0 +1,25 @@
+package com.example.fcmpractice.domain.user.facade;
+
+import com.example.fcmpractice.domain.user.domain.User;
+import com.example.fcmpractice.domain.user.domain.repository.UserRepository;
+import com.example.fcmpractice.domain.user.exception.AccountIdAlreadyExistsException;
+import com.example.fcmpractice.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getByAccountId(String accountId) {
+        return userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public void accountIdAlreadyExists(String accountId) {
+        if(userRepository.findByAccountId(accountId).isPresent())
+            throw AccountIdAlreadyExistsException.EXCEPTION;
+    }
+}
