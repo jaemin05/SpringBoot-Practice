35e1e3338919c7e04b96481c3fff1afe0b01935d
son <sonjm3518@dsm.hs.kr>
2022-03-21 23:46:34 +0900

    📑 :: Auth Details

---
+++ FCM-Practice/src/main/java/com/example/fcmpractice/global/security/auth/AuthDetails.java
@@ -0,0 +1,49 @@
+package com.example.fcmpractice.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final String accountId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return accountId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
