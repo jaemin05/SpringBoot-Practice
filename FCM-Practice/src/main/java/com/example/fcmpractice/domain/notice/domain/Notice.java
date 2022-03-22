69b9e82ef56aa715259fb4541b73a6f09459ff2a
son <sonjm3518@dsm.hs.kr>
2022-03-22 23:13:16 +0900

    📑 :: Notice Entity

---
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/notice/domain/Notice.java
@@ -0,0 +1,42 @@
+package com.example.fcmpractice.domain.notice.domain;
+
+import com.example.fcmpractice.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @Column(length = 200, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Notice(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+}
