c980f8153dc3e634393444a17e279f0f9ef886cf
son <sonjm3518@dsm.hs.kr>
2022-03-23 23:19:47 +0900

    ⚡ :: 메시지 수정

--- FCM-Practice/src/main/java/com/example/fcmpractice/domain/user/presentation/dto/LoginRequest.java
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/user/presentation/dto/LoginRequest.java
@@ -9,12 +9,12 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 public class LoginRequest {
 
-    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @NotBlank(message = "아이디는 필수 입력입니다.")
     private String accountId;
 
-    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력입니다.")
     private String password;
 
-    @NotBlank(message = "device_token은 필수 입력 값입니다.")
+    @NotBlank(message = "device_token은 필수 입력입니다.")
     private String device_token;
 }
