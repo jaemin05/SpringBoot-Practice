84d558ab3db209b9ae4cd93a5da533452cf7fa60
son <sonjm3518@dsm.hs.kr>
2022-03-23 23:38:33 +0900

    ⚡ :: @Builder 추가

--- FCM-Practice/src/main/java/com/example/fcmpractice/infrastructure/dto/NotificationRequest.java
+++ FCM-Practice/src/main/java/com/example/fcmpractice/infrastructure/dto/NotificationRequest.java
@@ -1,18 +1,19 @@
 package com.example.fcmpractice.infrastructure.dto;
 
+import com.example.fcmpractice.domain.notice.domain.Notice;
+import com.example.fcmpractice.domain.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Getter
-@NoArgsConstructor
+@Builder
 public class NotificationRequest {
-    @NotBlank(message = "title은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Size(max = 20, message = "title은 20자 까지 작성하실 수 있습니다.")
-    private String title;
 
-    @Size(max = 200, message = "content는 200자 까지 작성하실 수 있습니다.")
-    private String content;
+    private final User user;
+
+    private final String title;
+
+    private final String content;
+
+    private final Long data;
 }
