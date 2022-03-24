547e90c23cc6fafb3fb0afdaab796da3d8877ae0
son <sonjm3518@dsm.hs.kr>
2022-03-24 09:40:06 +0900

    📑 :: Notification List Entity

---
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/notification/domain/NotificationList.java
@@ -0,0 +1,34 @@
+package com.example.fcmpractice.domain.notification.domain;
+
+import com.example.fcmpractice.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class NotificationList {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notification_id")
+    private Notification notification;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public NotificationList(Notification notification, User user) {
+        this.notification = notification;
+        this.user = user;
+    }
+}
