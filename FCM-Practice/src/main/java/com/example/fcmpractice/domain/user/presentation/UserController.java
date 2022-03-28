345d71d61296822362711ea0def449ef0df2a35b
son <sonjm3518@dsm.hs.kr>
2022-03-28 20:40:16 +0900

    ⚡ :: @RequestBody 추가

--- FCM-Practice/src/main/java/com/example/fcmpractice/domain/user/presentation/UserController.java
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/user/presentation/UserController.java
@@ -7,8 +7,11 @@ import com.example.fcmpractice.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,12 +21,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public void signup(SignupRequest signupRequest) {
+    public void signup(@RequestBody @Valid SignupRequest signupRequest) {
         userService.signup(signupRequest);
     }
 
     @PatchMapping
-    public TokenResponse login(LoginRequest loginRequest) {
+    public TokenResponse login(@RequestBody @Valid LoginRequest loginRequest) {
         return userService.login(loginRequest);
     }
 }
