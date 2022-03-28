e4842b8dc65d70ee181c8111908399936fe908aa
son <sonjm3518@dsm.hs.kr>
2022-03-28 20:30:23 +0900

    ⚡ :: CrudRepository로 변경

--- FCM-Practice/src/main/java/com/example/fcmpractice/domain/notification/domain/repository/NotificationRepository.java
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/notification/domain/repository/NotificationRepository.java
@@ -1,7 +1,7 @@
 package com.example.fcmpractice.domain.notification.domain.repository;
 
 import com.example.fcmpractice.domain.notification.domain.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends CrudRepository<Notification, Long> {
 }
