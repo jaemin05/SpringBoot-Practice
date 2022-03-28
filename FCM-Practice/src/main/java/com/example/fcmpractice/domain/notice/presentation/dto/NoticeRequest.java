10cec639d5e555dc3b69aab38ae052abbcee9f5e
son <sonjm3518@dsm.hs.kr>
2022-03-28 20:26:54 +0900

    ⚡ :: @NoArgsConstructor 추가

--- FCM-Practice/src/main/java/com/example/fcmpractice/domain/notice/presentation/dto/NoticeRequest.java
+++ FCM-Practice/src/main/java/com/example/fcmpractice/domain/notice/presentation/dto/NoticeRequest.java
@@ -1,13 +1,13 @@
 package com.example.fcmpractice.domain.notice.presentation.dto;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class NoticeRequest {
     @NotBlank(message = "title은 필수 입력입니다.")
     @Size(min = 1, max = 30, message = "title은 1~30자리 이내로 입력해주세요.")
