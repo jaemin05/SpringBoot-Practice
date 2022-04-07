b16bf4b959f0ac43e3912bb3ac9a439e56cc2ccb
son <sonjm3518@dsm.hs.kr>
2022-04-07 23:39:48 +0900

    📑 :: SecurityConfig

--- OAuth/src/main/java/com/example/oauth/global/config/SecurityConfig.java
+++ OAuth/src/main/java/com/example/oauth/global/config/SecurityConfig.java
@@ -1,22 +1,28 @@
 package com.example.oauth.global.config;
 
+import com.example.oauth.domain.user.domain.Role;
+import com.example.oauth.global.oauth.service.CustomOAuth2UserService;
 import com.example.oauth.global.security.auth.CustomUserDetailsService;
 import com.example.oauth.global.security.token.TokenAuthenticationFilter;
 import com.example.oauth.global.security.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomOAuth2UserService customOAuth2UserService;
     private final TokenProvider tokenProvider;
 
     @Override
@@ -25,11 +31,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder());
     }
 
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors().and()
+                .csrf().disable()
+                .formLogin().disable()
 
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .antMatchers(HttpMethod.POST, "/**").hasRole(Role.USER.getRole())
+                .antMatchers(HttpMethod.GET, "/**").hasRole(Role.USER.getRole())
+                .anyRequest().authenticated()
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 
     @Override
