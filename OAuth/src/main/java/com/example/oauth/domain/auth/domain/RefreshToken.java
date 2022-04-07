0301d40146d13450e2bd3516c6d30ac8bea5286e
son <sonjm3518@dsm.hs.kr>
2022-04-07 23:31:47 +0900

    📑 :: RefreshToken

---
+++ OAuth/src/main/java/com/example/oauth/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.example.oauth.domain.auth.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+public class RefreshToken {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive
+    private Long ttl;
+
+    public void update(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
