932357b10aa7a0934de9a7acb967bc005564d0ca
son <sonjm3518@dsm.hs.kr>
2022-04-07 20:19:22 +0900

    ❌ :: Delete

--- OAuth/src/main/java/com/example/oauth/domain/refreshToken/UserRefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.oauth.domain.refreshToken;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, String> {
-    Optional<UserRefreshToken> findByRefreshToken(String refreshToken);
-}
