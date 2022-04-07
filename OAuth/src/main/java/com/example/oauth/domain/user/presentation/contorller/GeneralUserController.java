67c3932646cbee0fd4a259f02f1c984b4abb2533
son <sonjm3518@dsm.hs.kr>
2022-04-07 23:36:58 +0900

    ⚡ :: reissue 삭제

--- OAuth/src/main/java/com/example/oauth/domain/user/presentation/contorller/GeneralUserController.java
+++ OAuth/src/main/java/com/example/oauth/domain/user/presentation/contorller/GeneralUserController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth/general")
+@RequestMapping("/generalUser")
 public class GeneralUserController {
     private final GeneralUserService userService;
 
@@ -28,10 +27,5 @@ public class GeneralUserController {
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         return ResponseEntity.ok(userService.login(loginRequest));
-    }
-
-    @PostMapping("/reissue")
-    public TokenResponse reissue(@RequestHeader("X-Refresh-Token") String refresh) {
-        return userService.reissue(refresh);
     }
 }
