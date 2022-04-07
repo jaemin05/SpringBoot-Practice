f130994e00776d4063c46a5d0e9a3ff5d8e9c68e
son <sonjm3518@dsm.hs.kr>
2022-04-07 23:39:30 +0900

    ⚡ :: header, prefix 추가

--- OAuth/src/main/java/com/example/oauth/global/config/JwtProperties.java
+++ OAuth/src/main/java/com/example/oauth/global/config/JwtProperties.java
@@ -4,16 +4,25 @@ import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Getter
 @ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    private final Auth auth = new Auth();
 
-    @Getter
-    public static class Auth {
-        private String secretToken;
-        private long accessTokenExp;
-        private long refreshTokenExp;
+    private final String secretKey;
+    private final Long accessExp;
+    private final Long refreshExp;
+    private final String header;
+    private final String prefix;
+
+    public JwtProperties(String secretKey, Long accessExp, Long refreshExp, String header, String prefix) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.accessExp = accessExp;
+        this.refreshExp = refreshExp;
+        this.header = header;
+        this.prefix = prefix;
     }
 }
