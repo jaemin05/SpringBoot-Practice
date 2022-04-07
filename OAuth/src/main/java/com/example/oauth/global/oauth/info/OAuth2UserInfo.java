0d5e42a38d102b5615f4190b9c33907d87087a74
son <sonjm3518@dsm.hs.kr>
2022-04-07 22:51:03 +0900

    📑 :: OAuth2UserInfo

--- OAuth/src/main/java/com/example/oauth/global/oauth/info/OAuth2UserInfo.java
+++ OAuth/src/main/java/com/example/oauth/global/oauth/info/OAuth2UserInfo.java
@@ -1,21 +1,30 @@
 package com.example.oauth.global.oauth.info;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Map;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class OAuth2UserInfo {
-    protected Map<String, Object> attributes;
 
-    public OAuth2UserInfo(Map<String, Object> attributes) {
-        this.attributes = attributes
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String profileImageUrl;
+
+    @Builder
+    public OAuth2UserInfo(Map<String, Object> attributes, String nameAttributeKey,
+                          String name, String email, String profileImageUrl) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.profileImageUrl = profileImageUrl;
     }
-
-    public Map<String, Object> getAttributes() {
-        return attributes;
-    }
-
-    public abstract String getName;
-
-    public abstract String getEmail;
-
-    public abstract String profileImageUrl;
 }
