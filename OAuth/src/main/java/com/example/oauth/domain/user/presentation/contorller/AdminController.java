a414bf69bc69dfe7691bea9ce502ebf0c9976d00
son <sonjm3518@dsm.hs.kr>
2022-04-07 23:36:26 +0900

    ⚡ :: Auth Controller

--- OAuth/src/main/java/com/example/oauth/domain/user/presentation/contorller/AdminController.java
+++ OAuth/src/main/java/com/example/oauth/domain/user/presentation/contorller/AdminController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth/admin")
+@RequestMapping("/admin")
 public class AdminController {
     private final AdminService adminService;
 
@@ -28,10 +27,5 @@ public class AdminController {
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         return ResponseEntity.ok(adminService.login(loginRequest));
-    }
-
-    @PostMapping("/reissue")
-    public TokenResponse reissue(@RequestHeader("X-Refresh-Token") String refresh) {
-        return adminService.reissue(refresh);
     }
 }
